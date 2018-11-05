f68355c5c5e4e66a0a2774c7d19862480edbfd73
Martynas Kanapinskas <Martis347@gmail.com>
2018-11-05 21:34:33 +0200

    Updates for vuetify

--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -1,6 +1,9 @@
+import Vuetify, {
+  VLayout, VApp, VCard, VBtn, VSubheader, VFlex, VIcon, VCardText, VListTileSubTitle, VListTileTitle, VListTileContent, VCheckbox, VListTileAction, VListTile, VList, VCardTitle, VDialog, VDivider, VTextField, VContent,
+} from 'vuetify/lib';
 import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.css';
+import * as directives from 'vuetify/es5/directives';
+import '../../node_modules/vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify, {
   theme: {
@@ -9,5 +12,9 @@ Vue.use(Vuetify, {
     accent: '#8c9eff',
     error: '#b71c1c',
   },
+  components: {
+    VLayout, VApp, VCard, VBtn, VSubheader, VFlex, VIcon, VCardText, VListTileSubTitle, VListTileTitle, VListTileContent, VCheckbox, VListTileAction, VListTile, VList, VCardTitle, VDialog, VDivider, VTextField, VContent,
+  },
+  iconfont: 'md',
+  directives,
 });
-
