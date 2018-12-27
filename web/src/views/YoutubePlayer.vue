3cc80fba36e4b23d6036419d5778f10dc53e07f1
Martynas Kanapinskas <martynas.kanapinskas@gmail.com>
2018-12-28 01:29:45 +0200

    Completed vuex usage for all components

--- web/src/views/YoutubePlayer.vue
+++ web/src/views/YoutubePlayer.vue
@@ -13,7 +13,7 @@ export default {
     player: null,
     showPlayer: false,
     playedVideoId: '',
-    last5VideoIds: [],
+    last3VideoIds: [],
   }),
   mounted() {
     this.showPlayer = Object.prototype.hasOwnProperty.call(this.$route.query, 'show');
@@ -25,7 +25,7 @@ export default {
     this.player.on('stateChange', async (event) => {
       if (event.data === 0) {
         const { data } = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${this.playedVideoId}&type=video&key=AIzaSyDfLd9pl_DpU84NvwXznFkmUsjM9kiiAiI`);
-        const videoIds = data.items.map(i => i.id.videoId).filter(id => !this.last5VideoIds.includes(id));
+        const videoIds = data.items.map(i => i.id.videoId).filter(id => !this.last3VideoIds.includes(id));
         const nextVideoId = videoIds[this.getRandomInt(0, videoIds.length - 1)];
         this.playVideo(nextVideoId);
       }
@@ -43,10 +43,10 @@ export default {
       console.log(id);
       this.player.loadVideoById(id);
       this.playedVideoId = id;
-      this.last5VideoIds.push(id);
+      this.last3VideoIds.push(id);
       this.$router.push({ params: { videoId: id } });
-      if (this.last5VideoIds.length === 5) {
-        this.last5VideoIds = this.last5VideoIds.slice(1);
+      if (this.last3VideoIds.length === 3) {
+        this.last3VideoIds = this.last3VideoIds.slice(1);
       }
     },
     stop() {
